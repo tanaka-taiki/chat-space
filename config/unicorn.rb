cd02c7f37f61bd645506ac44c61f3d67a178d502
tanaka-taiki <tanakataiki@tanakadaikinoMacBook-puro.local>
2018-10-10 20:22:35 +0900

    Five

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
 app_path = File.expand_path('../../../', __FILE__)
 worker_processes 1
 
-working_directory "#{chat-space}/current"
-listen "#{chat-space}/shared/tmp/sockets/unicorn.sock"
-pid "#{chat-space}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{chat-space}/shared/log/unicorn.stderr.log"
-stdout_path "#{chat-space}/shared/log/unicorn.stdout.log"
+working_directory "#{app_path}/current"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
