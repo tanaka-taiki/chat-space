48a1f3191485eb3aba02eec6a0249d2eb022bf35
tanaka-taiki <tanakataiki@tanakadaikinoMacBook-puro.local>
2018-10-11 17:32:59 +0900

    Edit imageuploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,10 +2,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  
-  storage :fog
 
   process resize_to_fit: [800, 800]
+  storage :fog
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
