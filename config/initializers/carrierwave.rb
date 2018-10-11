cb6ac010fa00a82b7e291f34d4e4b4edb8ffe3dd
tanaka-taiki <tanakataiki@tanakadaikinoMacBook-puro.local>
2018-10-11 17:27:56 +0900

    Edit S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
+  config.fog_directory  = 'kaedama-test'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kaedama-test'
 end
